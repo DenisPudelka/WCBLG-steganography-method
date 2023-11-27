0ba1b131bb4ec558b0cbe97c87826857c50b98fc
Denis <denispudelka@gmail.com>
2023-11-27 16:29:07 +0100

    Added necessary changes.

--- main.py
+++ main.py
@@ -2,53 +2,58 @@ from Utils import *
 from WCBLGAlgorithm import WCBLGAlgorithm
 from WCBLGExtraction import WCBLGExtraction
 import tifffile
-import cv2
 
-def write_seeds_to_file(seeds):
-    with open('seeds.txt', 'w') as file:
-        file.write('\n'.join(str(seed) for seed in seeds))
 
-def read_seeds_from_file():
-    file1 = open('seeds.txt', 'r')
-    Lines = file1.readlines()
+def encrypt(image_path, key, Bs, mul, Npop, Pc, Pm, Epoch):
+    # read image
+    image_original = tifffile.imread(image_path)
 
-    seeds = []
-    for line in Lines:
-        if line is None:
-            break
-        seeds.append(int(line.strip()))
-    return seeds
+    # convert image to grayscale
+    cover_image = color_to_gray_matlab(image_original)
+
+    # read message
+    data = read_message("message/Lorem Ipsum 100B.txt")
+
+    # calling embedding algorithm
+    wcblgEmbedding = WCBLGAlgorithm(cover_image, data, key, Bs, mul, Npop, Pc, Pm, Epoch)
+    bestSeeds, stego_image, tags = wcblgEmbedding.wcblg()
+    print(bestSeeds)
+
+    # save seeds, cover image and stego image
+    write_seeds_to_file(bestSeeds, "seeds_1.txt")
+    save_image(cover_image, "cover_image/cover_image_1.tif")
+    save_image(stego_image, "stego_image/stego_image_1.tif")
+
+
+def decrypt(key, Bs, mul):
+    # read image
+    stego_image = tifffile.imread("stego_image/stego_image_1.tif")
+
+    # read best seeds
+    bestSeeds = read_seeds_from_file("seeds_1.txt")
+
+    # read message and get length in bin
+    data = read_message("message/Lorem Ipsum 100B.txt")
+    data_bin = string_to_bin(data)
+
+    # calling extraction algorithm
+    wcblgExtraction = WCBLGExtraction(stego_image, key, Bs, mul, bestSeeds, len(data_bin))
+    hidden_message = wcblgExtraction.extract_data()
+
+    print(hidden_message)
 
 
 if __name__ == '__main__':
-    image_path = "128x128PNG.png"
+    image_path = "original_images/peppers_color.tiff"
 
-
-    #data = "Nullam aliquet pellentesque ligula ut consectetur. Sed ac ante lacinia, vestibulum tortor in, vulputate nulla. Nunc viverra tempor orci a scelerisque. Pellentesque id tortor commodo, dignissim massa vitae, elementum tellus. Integer lectus sapien, tincidunt sit amet risus sed, vulputate gravida ex. Donec finibus, nibh in iaculis malesuada, est arcu gravida libero, sit amet luctus magna augue in tortor. Nunc ornare ut ipsum nec malesuada. Sed eget suscipit sapien. Mauris suscipit quam a ligula vulputate vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis eros eu libero accumsan scelerisque. Nullam pharetra, mauris quis dignissim mollis, turpis nisi aliquet mi, a faucibus dolor enim vel lacus. Nullam aliquet pellentesque ligula ut consectetur. Sed ac ante lacinia, vestibulum tortor in, vulputate nulla."
-    data = ""
-    for i in range(0, 10):
-        data += "Denis Pudelka123"
     key = 12345
-    Bs = 16
+    Bs = 256
     mul = 1.2
     Npop = 20
     Pc = 0.7
     Pm = 0.2
     Epoch = 20
 
-    #cover_image = tifffile.imread(image_path)
-    cover_image = cv2.imread(image_path, 0)
+    encrypt(image_path, key, Bs, mul, Npop, Pc, Pm, Epoch)
 
-    wcblgEmbedding = WCBLGAlgorithm(image_path, data, key, Bs, mul, Npop, Pc, Pm, Epoch)
-    bestSeeds, stego_image, tags = wcblgEmbedding.wcblg()
-    write_seeds_to_file(bestSeeds)
-    print(bestSeeds)
-    #tifffile.imwrite("stego.tif", stego_image, **tags)
-    cv2.imwrite("cover.png", cover_image)
-    cv2.imwrite("stego.png", stego_image)
-    stego_image = cv2.imread("stego.png", 0)
-    #bestSeeds = read_seeds_from_file()
-
-    data_bin = string_to_bin(data)
-    wcblgExtraction = WCBLGExtraction(stego_image, key, Bs, mul, bestSeeds, len(data_bin))
-    print(wcblgExtraction.extract_data())
+    decrypt(key, Bs, mul)
