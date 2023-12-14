19cc97805c2ded38fc51b61513de635e5ff5f909
Denis <denispudelka@gmail.com>
2023-12-14 14:45:19 +0100

    Created some function that could embed into DWT coefts.

--- testNewEmbeddingMethod.py
+++ testNewEmbeddingMethod.py
@@ -31,6 +31,15 @@ def increment_float_dynamic(num):
     increment = 10 ** (-decimal_places)
     return round(num + increment, decimal_places)
 
+
+def decrement_float_dynamic(num):
+    decimal_places = str(num)[::-1].find('.')
+    decrement = 10 ** (-decimal_places)
+    decremented_num = round(num - decrement, decimal_places)
+    if decremented_num == 0 and decimal_places > 0:
+        decremented_num = round(num - (decrement / 10), decimal_places)
+    return decremented_num
+
 def increment_float_dynamic_2(num):
     num_str = '{:.15f}'.format(num).rstrip('0')
     decimal_places = num_str[::-1].find('.')
@@ -38,6 +47,17 @@ def increment_float_dynamic_2(num):
     incremented_num = round(num + increment, decimal_places)
     #return format(incremented_num, f'.{decimal_places}f')
     return incremented_num
+
+
+def decrement_float_dynamic_2(num):
+    num_str = '{:.15f}'.format(num).rstrip('0')
+    decimal_places = num_str[::-1].find('.')
+    decrement = 10 ** (-decimal_places)
+    decremented_num = round(num - decrement, decimal_places)
+    if decremented_num == 0 and decimal_places > 0:
+        decremented_num = round(num - (decrement / 10), decimal_places)
+    return format(decremented_num, f'.{decimal_places}f')
+
 
 # def embedding(HH, HHprim, can_loc, best_seed, data_k, mul, HH_keys):
 #     np.random.seed(int(best_seed))
@@ -55,7 +75,7 @@ def increment_float_dynamic_2(num):
 #         d += 1
 #     return HHS
 
-num = 0.000000000009
+num = 0.00010
 bin_representation = float_to_bin(num)
 newNum = bin_to_float(bin_representation)
 modyNum = modify_lsb_of_float(num, 1)
@@ -68,3 +88,5 @@ print("Modified LSB to binary: ", float_to_bin(modyNum))
 print("Incrementing by the smallest amount using math library: number:", increment_float(num), " binary: ", float_to_bin(increment_float(num)))
 print("Incrementing by the smallest amount using numpy library: number:", increment_float_numpy(num), " binary: ", float_to_bin(increment_float(num)))
 print("Incrementing by the smallest amount dynamically: number:", increment_float_dynamic_2(num), " binary: ", float_to_bin(increment_float(num)))
+print("Decrement by the smallest amount dynamically: number:", decrement_float_dynamic(num), " binary: ", float_to_bin(increment_float(num)))
+print("Decrement by the smallest amount dynamically: number:", decrement_float_dynamic_2(num), " binary: ", float_to_bin(increment_float(num)))
