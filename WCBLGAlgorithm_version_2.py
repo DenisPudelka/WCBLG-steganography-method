80136af5cfaba0af4e99ce1012adbf607c73a180
Denis <denispudelka@gmail.com>
2024-01-29 12:53:41 +0100

    Implemented embedding message into HL and LH.

--- WCBLGAlgorithm_version_2.py
+++ WCBLGAlgorithm_version_2.py
@@ -5,7 +5,7 @@ import numpy as np
 import math
 import cv2
 from Utils import *
-from Embedding import embedding
+from Embedding_version_2 import embedding
 from GeneticAlgorithm import GeneticAlgorithm
 import tifffile
 
@@ -22,96 +22,87 @@ class WCBLGAlgorithm:
         self.pc = pc
         self.pm = pm
         self.epoch = epoch
-        self.len_data = None
-        self.can_loc = None
         self.LL = None
         self.LH = None
         self.HL = None
         self.HH = None
-        self.subband_prim = None
         self.cover_k = None
-        self.data_k = None
-        self.subband_keys = {}
         self.eng = eng
         self.use_iwt = use_iwt
-        self.data_bin_by_subband = []
+        self.m, self.n = self.cover_image.shape
+        self.max_capacity_per_subband = int(self.m * self.n / (4 * self.mul))
+        self.data_bin_HH = None  # binary data for HH subband
+        self.data_bin_HL = None
+        self.data_bin_LH = None
+        self.len_data_HH = 0    # len of binary data
+        self.len_data_HL = 0
+        self.len_data_LH = 0
+        self.len_data_HH_block = 0  # len of binary data per block
+        self.len_data_HL_block = 0
+        self.len_data_LH_block = 0
+        self.NumRows = self.m // self.BS
+        self.NumCols = self.n // self.BS
 
-
-    def number_of_quarters(self, L):
-        m, n = self.cover_image.shape
-        if L < m * n / (4 * self.mul):
-            return 1
-        if L < 2 * m * n / (4 * self.mul):
-            return 2
-        if L < 3 * m * n / (4 * self.mul):
-            return 3
-        return -1
-
-
-    def get_subband_s(self, BestSeeds, subband):
-        # for HH
-        # selekcija lokacije za embedovanje
-        self.SelEmbLocForSubband(subband)
-
-        # GA
-        genericAlgorithm = GeneticAlgorithm(self.n_pop, self.pc, self.pm, self.epoch, self.can_loc,
-                                            self.cover_k, self.LL, self.LH, self.HL, self.HH, self.subband_prim,
-                                            self.data_k, self.mul, self.key, self.subband_keys, self.eng, self.use_iwt)
-        bestseedk = genericAlgorithm.findBestKey()  # treba da se popravi
-        BestSeeds.append(bestseedk)
-
-        # Embedovanje data
-        subband_s = embedding(subband, self.subband_prim, self.can_loc, bestseedk, self.data_k, self.mul, self.subband_keys,
-                        self.use_iwt)
-        return subband_s
-
-
-    def extract_data_by_subbands(self, number_of_quarters):
+    def prepare_algorith(self):
         data_bin = string_to_bin(self.data)
-        L = len(data_bin)
-        subband_part = L / number_of_quarters
-        for i in range(number_of_quarters):
-            self.data_bin_by_subband.append(data_bin[i * subband_part : (i+1) * subband_part])
+        data_len = len(data_bin)
 
+        block_number = self.NumRows * self.NumCols
+
+        # if numbers are not divisible , reminder will be lost which means part of message is not embeded, or we fill message with spaces
+        # if data_len % block_number != 0:
+        #     pass
+        # self.len_data = data_len // block_number
+
+        self.stego_image = np.zeros_like(self.cover_image, dtype=self.cover_image.dtype)  # tu mozno dam dtype=image.dtype
+
+        if data_len <= self.max_capacity_per_subband:
+            self.data_bin_HH = data_bin
+            self.len_data_HH = data_len
+            self.len_data_HH_block = self.len_data_HH // block_number
+            return True
+        if data_len <= 2 * self.max_capacity_per_subband:
+            self.data_bin_HH = data_bin[0: self.max_capacity_per_subband]
+            self.len_data_HH = self.max_capacity_per_subband
+            self.len_data_HH_block = self.len_data_HH // block_number
+            if self.len_data_HH % block_number != 0:
+                print("max len data per block not divible by block number")
+                return False
+            self.data_bin_HL = data_bin[self.max_capacity_per_subband:]
+            self.data_bin_HL = data_len - self.max_capacity_per_subband
+            self.len_data_HL_block = self.len_data_HL // block_number
+            return True
+        if data_len <= 3 * self.max_capacity_per_subband:
+            self.data_bin_HH = data_bin[0: self.max_capacity_per_subband]
+            self.len_data_HH = self.max_capacity_per_subband
+            self.len_data_HH_block = self.len_data_HH // block_number
+            if self.len_data_HH % block_number != 0:
+                print("max len data per block not divible by block number")
+                return False
+            self.data_bin_HL = data_bin[self.max_capacity_per_subband: 2 * self.max_capacity_per_subband]
+            self.len_data_HL = self.max_capacity_per_subband
+            self.len_data_HL_block = self.len_data_HL // block_number
+            if self.len_data_HL % block_number != 0:
+                print("max len data per block not divible by block number")
+                return False
+            self.data_bin_LH = data_bin[2 * self.max_capacity_per_subband:]
+            self.len_data_LH = data_len - 2 * self.max_capacity_per_subband
+            self.len_data_LH_block = self.len_data_LH // block_number
+            return True
+        if data_len > 3 * self.max_capacity_per_subband:
+            print("Not enough space")
+            return False
 
     def wcblg(self):
-        data_bin = string_to_bin(self.data)
-        l = len(data_bin)
-        number_of_quarters = self.number_of_quarters(l)
-        self.extract_data_by_subbands(number_of_quarters)
-
-        if number_of_quarters > 0:
-            self.wcblg_for_subband(self.HH, self.data_bin_by_subband[0])
-        elif number_of_quarters > 1:
-            self.wcblg_for_subband()
-        elif number_of_quarters > 2:
-            self.wcblg_for_subband()
-        else:
-            return  None
-
-    def wcblg_for_subband(self, number_of_quarters):
-        m, n = self.cover_image.shape
-        data_bin  = []
-
-        data_bin = string_to_bin()
-
-        NumRows = m // self.BS
-        NumCols = n // self.BS
-
-        block_number = NumRows * NumCols
-
-        L = len(data_bin)
-        self.len_data = L // (block_number)  # if numbers are not divisible , reminder will be lost which means part of message is not embeded, or we fill message with spaces
 
         k = 1
-        self.stego_image = np.zeros_like(self.cover_image, dtype=self.cover_image.dtype) # tu mozno dam dtype=image.dtype
         BestSeeds = []
 
-
-        for i in range(NumRows):
-            for j in range(NumCols):
+        for i in range(self.NumRows):
+            for j in range(self.NumCols):
                 # Extrakcia blokova
-                self.GetSubBlCoverK( i, j)
+                # prepares cover_k
+                self.GetSubBlCoverK(i, j)
 
                 # DWT or IWT transformacija
                 if self.use_iwt:
@@ -119,22 +110,44 @@ class WCBLGAlgorithm:
                 else:
                     self.LL, self.LH, self.HL, self.HH = DWT_version_2(self.cover_k, self.eng)
 
-                HHS = self.HH
                 HLS = self.HL
                 LHS = self.LH
-                if number_of_quarters > 0:
-                    self.GetSubBlDataK(self.data_bin_by_subband[0], k)
-                    HHS = self.get_subband_s(BestSeeds, self.HH)
-                elif number_of_quarters > 1:
-                    self.GetSubBlDataK(self.data_bin_by_subband[1], k)
-                    HLS = self.get_subband_s(BestSeeds, self.HL)
-                elif number_of_quarters > 2:
-                    self.GetSubBlDataK(self.data_bin_by_subband[2], k)
-                    LHS = self.get_subband_s(BestSeeds, self.LH)
+                data_k_HL = None
+                can_loc_HL = None
+                HL_prim = None
+                HL_keys = None
+
+                data_k_LH = None
+                can_loc_LH = None
+                LH_prim = None
+                LH_keys = None
 
 
-                subband_s = self.get_subband_s(BestSeeds, subband=0)
+                data_k_HH = self.GetSubBlDataK(self.data_bin_HH, self.len_data_HH_block, k)
+                can_loc_HH, HH_prim, HH_keys = self.SelEmbLocForSubband(self.HH, self.len_data_HH_block)
 
+                if self.data_bin_HL is not None:
+                    data_k_HL = self.GetSubBlDataK(self.data_bin_by_HL, self.len_data_HL_block, k)
+                    can_loc_HL, HL_prim, HL_keys = self.SelEmbLocForSubband(self.HL, self.len_data_HL_block)
+
+                if self.data_bin_LH is not None:
+                    data_k_LH = self.GetSubBlDataK(self.data_bin_by_LH, self.len_data_LH_block, k)
+                    can_loc_LH, LH_prim, LH_keys = self.SelEmbLocForSubband(self.LH, self.len_data_LH_block)
+
+
+                genericAlgorithm = GeneticAlgorithm(self.n_pop, self.pc, self.pm, self.epoch, can_loc_HH, can_loc_HL, can_loc_LH,
+                                                    self.cover_k, self.LL, self.LH, self.HL, self.HH, HH_prim, HL_prim, LH_prim,
+                                                    data_k_HH, data_k_HL, data_k_LH, self.mul, self.key, HH_keys, HL_keys, LH_keys, self.eng,
+                                                    self.use_iwt)
+                bestseedk = genericAlgorithm.findBestKey()  # treba da se popravi
+                BestSeeds.append(bestseedk)
+
+                # Embedovanje data
+                HHS = embedding(self.HH, HH_prim, can_loc_HH, bestseedk, data_k_HH, self.mul, HH_keys, self.use_iwt)
+                if data_k_HL is not None:
+                    HLS = embedding(self.HL, HL_prim, can_loc_HL, bestseedk, data_k_HL, self.mul, HL_keys, self.use_iwt)
+                if data_k_LH is not None:
+                    LHS = embedding(self.LH, LH_prim, can_loc_LH, bestseedk, data_k_LH, self.mul, LH_keys, self.use_iwt)
 
                 # IDWT or IIWT transformacija
                 if self.use_iwt:
@@ -150,23 +163,23 @@ class WCBLGAlgorithm:
         return BestSeeds, self.stego_image
 
 
-    def SelEmbLocForSubband(self, subband):
+    def SelEmbLocForSubband(self, subband, len_data_block):
         seed(self.key)
         n, m = subband.shape
-        self.subband_keys = {}
-        self.subband_prim = np.zeros((n, m))
+        subband_keys = {}
+        subband_prim = np.zeros((n, m))
         for i, item in enumerate(subband):
             for j, num in enumerate(item):
                 r = random.random()
-                self.subband_keys[str(i) + "," + str(j)] = r
+                subband_keys[str(i) + "," + str(j)] = r
                 num_int = int(round(num))
                 if num_int % 2 == 0:
-                    self.subband_prim[i, j] = num_int     # previously here was num instead of num_int
+                    subband_prim[i, j] = num_int     # previously here was num instead of num_int
                 else:
                     if r <= 0.5:
-                        self.subband_prim[i, j] = num_int + 1     # previously here was num instead of num_int
+                        subband_prim[i, j] = num_int + 1     # previously here was num instead of num_int
                     else:
-                        self.subband_prim[i, j] = num_int - 1     # previously here was num instead of num_int
+                        subband_prim[i, j] = num_int - 1     # previously here was num instead of num_int
         edges = np.zeros((n, m))
         for i in range(n):
             for j in range(m):
@@ -174,25 +187,25 @@ class WCBLGAlgorithm:
                     for y in range(-1, 2):
                         if i + x < 0 or i + x >= n or j + y < 0 or j + y >= m:
                             continue
-                        edges[i, j] += abs(self.subband_prim[i + x + 1, j + y + 1])
+                        edges[i, j] += abs(subband_prim[i + x, j + y]) # edges[i, j] += abs(subband_prim[i + x + 1, j + y + 1])
         edges_array = edges.flatten()
         # edges_array[::-1].sort() ovo moze da radi problem jer nikde necuvamo sortirani array
         edges_array.sort()
         edges_array = edges_array[::-1]
 
-        element_number = math.ceil(self.mul * self.len_data)
+        element_number = math.ceil(self.mul * len_data_block)
         if len(edges_array) < element_number:
             print("No place for embedding.")
             return None, None
         treshold = edges_array[element_number - 1]
 
-        self.can_loc = []
+        can_loc = []
         elements_found = 0
         break_loop = False
         for i in range(n):
             for j in range(m):
                 if edges[i, j] >= treshold:
-                    self.can_loc.append((i, j))
+                    can_loc.append((i, j))
                     elements_found += 1
                 if elements_found == element_number:
                     break_loop = True
@@ -200,7 +213,7 @@ class WCBLGAlgorithm:
             if break_loop:
                 break
 
-        return self.can_loc, self.subband_prim  # returns locations in HH with hihgest edge and HHPrim(like HH but all even)
+        return can_loc, subband_prim, subband_keys  # returns locations in HH with hihgest edge and HHPrim(like HH but all even)
 
 
     def GetSubBlCoverK(self, i, j):
@@ -210,16 +223,8 @@ class WCBLGAlgorithm:
         Endj = (j + 1) * self.BS
         self.cover_k = self.cover_image[Starti: Endi, Startj: Endj]
 
-    def GetSubBlDataK(self, data_bin, k):
-        self.data_k = data_bin[(k - 1) * self.len_data: k * self.len_data]
-
-    def GetSubBl(self, data_bin, i, j, k):
-        Starti = i * self.BS
-        Startj = j * self.BS
-        Endi = (i + 1) * self.BS
-        Endj = (j + 1) * self.BS
-        self.cover_k = self.cover_image[Starti: Endi, Startj: Endj]
-        self.data_k = data_bin[(k - 1) * self.len_data: k * self.len_data]
+    def GetSubBlDataK(self, data_bin, len_data_block,  k):
+        return data_bin[(k - 1) * len_data_block: k * len_data_block]
 
     def SetSubBl(self, stego_k, i, j):
         Starti = i * self.BS
