6a9575ecc81efbc4ff5574e3b4ff0b3a6273b149
Denis <denispudelka@gmail.com>
2024-02-13 17:50:45 +0100

    Small fix

--- main_ui.py
+++ main_ui.py
@@ -1,8 +1,4 @@
-from ui.steganography_ui import Steganography_UI
 from ui.gui import GUI
-
-# app = Steganography_UI()
-# app.mainloop()
 
 app = GUI()
 app.mainloop()
--- ui/gui.py
+++ ui/gui.py
@@ -11,8 +11,8 @@ class GUI(tk.Tk):
 
     def init_widgets(self):
         self.resizable(False,False)
-        self.main_freme = tk.LabelFrame(self, text="Steganography Mode", padx=50, pady=50)
-        self.main_freme.pack(padx=40, pady=40)
+        self.main_freme = tk.LabelFrame(self, text="Steganography Mode", padx=25, pady=25)
+        self.main_freme.pack(padx=10, pady=10)
 
         self.embed_button_mode = tk.Button(self.main_freme, text="Embed Mode", command=self.open_embed_window)
         self.embed_button_mode.pack(side=tk.LEFT, padx=10, pady=10)
