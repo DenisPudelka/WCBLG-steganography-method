3dd6dcf85e0349cd7d815532a8105db607ccaa2c
Denis <denispudelka@gmail.com>
2024-02-13 17:47:06 +0100

    Added option to call ExtractMode.

--- ui/gui.py
+++ ui/gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from ui.embed import EmbedMode
+from ui.extract import ExtractMode
 
 class GUI(tk.Tk):
     def __init__(self):
@@ -16,11 +17,11 @@ class GUI(tk.Tk):
         self.embed_button_mode = tk.Button(self.main_freme, text="Embed Mode", command=self.open_embed_window)
         self.embed_button_mode.pack(side=tk.LEFT, padx=10, pady=10)
 
-        self.extract_button_mode = tk.Button(self.main_freme, text="Extract Mode")
+        self.extract_button_mode = tk.Button(self.main_freme, text="Extract Mode", command=self.open_extract_window)
         self.extract_button_mode.pack(side=tk.RIGHT, padx=10, pady=10)
 
     def open_embed_window(self):
         EmbedMode()
 
     def open_extract_window(self):
-        pass
+        ExtractMode()
