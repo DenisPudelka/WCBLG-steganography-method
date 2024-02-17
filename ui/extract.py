7a3fb64bd82440c8a8307bfe1480aa7066c1f224
Denis <denispudelka@gmail.com>
2024-02-17 10:38:21 +0100

    Small fix.

--- ui/extract.py
+++ ui/extract.py
@@ -30,22 +30,22 @@ class ExtractMode:
         tk.Label(self.parameters_frame, text="Key: ").grid(row=1, column=0, sticky="e")
         self.key_entry = tk.Entry(self.parameters_frame)
         self.key_entry.grid(row=1, column=1, sticky="w", pady=5)
-        self.key_entry.insert(0, "e.g., 12345")
+        self.key_entry.insert(0, "12345")
 
         tk.Label(self.parameters_frame, text="Block Size (Bs): ").grid(row=2, column=0, sticky="e")
         self.bs_entry = tk.Entry(self.parameters_frame)
         self.bs_entry.grid(row=2, column=1, sticky="w", pady=5)
-        self.bs_entry.insert(0, "e.g., 256")
+        self.bs_entry.insert(0, "256")
 
         tk.Label(self.parameters_frame, text="Multiplier (Mul): ").grid(row=3, column=0, sticky="e")
         self.mul_entry = tk.Entry(self.parameters_frame)
         self.mul_entry.grid(row=3, column=1, sticky="w", pady=5)
-        self.mul_entry.insert(0, "e.g., 1.2")
+        self.mul_entry.insert(0, "1.2")
 
         tk.Label(self.parameters_frame, text="Length of embedded data: ").grid(row=4, column=0, sticky="e")
         self.pop_size_entry = tk.Entry(self.parameters_frame)
         self.pop_size_entry.grid(row=4, column=1, sticky="w", pady=5)
-        self.pop_size_entry.insert(0, "e.g., 50")
+        self.pop_size_entry.insert(0, "50")
 
     def transformation_frame_widgets(self):
         self.transformation_frame = tk.LabelFrame(self.window_frame)
