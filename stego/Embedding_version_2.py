4ad65403097edce106bc27945d8cc136d6bb0165
Denis <denispudelka@gmail.com>
2024-02-25 11:28:15 +0100

    Refactoring.

--- stego/Embedding_version_2.py
+++ stego/Embedding_version_2.py
@@ -6,43 +6,29 @@ import struct
 from stego.Utils import *
 
 
-def embedding(subband, subband_prim, can_loc, best_seed, data_k, mul, subband_keys, use_iwt):
+def embedding(subband, subband_prim, can_loc, best_seed, data_k, mul, subband_keys):
     seed(int(best_seed))
     lenData = len(data_k)
     element_number = math.ceil(mul * lenData)
     seq = random.sample(range(0, element_number), lenData)
-    # best_loc = can_loc[seq]  # this is a matrix of pair sekvence [(1,2),(2,2)]
+
     if element_number != len(can_loc):
         raise Exception("element number and can_loc are not the same length")
     best_loc = [can_loc[i] for i in seq]
     d = 0
     subband_S = subband
-    if use_iwt:
-        for i, j in best_loc:
-            data_part = int(data_k[d])
-            num = int(round(subband_prim[i, j]))
-            if num % 2 != data_part:
 
-                r = subband_keys[str(i) + "," + str(j)]
-                if r <= 0.5:
-                    subband_S[i, j] = subband_prim[i, j] - 1
-                else:
-                    subband_S[i, j] = subband_prim[i, j] + 1
-            else:
-                subband_S[i, j] = subband_prim[i, j]
-            d += 1
-    else:
-        for i, j in best_loc:
-            data_part = int(data_k[d])
-            num = int(round(subband_prim[i, j]))
-            if num % 2 != data_part:
+    for i, j in best_loc:
+        data_part = int(data_k[d])
+        num = int(round(subband_prim[i, j]))
+        if num % 2 != data_part:
 
-                r = subband_keys[str(i) + "," + str(j)]
-                if r <= 0.5:
-                    subband_S[i, j] = subband_prim[i, j] - 1
-                else:
-                    subband_S[i, j] = subband_prim[i, j] + 1
+            r = subband_keys[str(i) + "," + str(j)]
+            if r <= 0.5:
+                subband_S[i, j] = subband_prim[i, j] - 1
             else:
-                subband_S[i, j] = subband_prim[i, j]
-            d += 1
+                subband_S[i, j] = subband_prim[i, j] + 1
+        else:
+            subband_S[i, j] = subband_prim[i, j]
+        d += 1
     return subband_S
